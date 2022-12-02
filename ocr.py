4c9b52953077af0720905568814782ac0589d8cd
caklutfi <lutfytalker@gmail.com>
2022-12-03 00:43:35 +0700

    ocr commit pertama

---
+++ ocr.py
@@ -0,0 +1,55 @@
+import ocrspace
+import requests
+from pprint import pprint
+
+
+apikey='K89677753388957'
+
+api = ocrspace.API()
+# Or if you have a custom API host, API key or desired language, pass those:
+# api = ocrspace.API(endpoint='https://api.ocr.space/parse/image', api_key='apikey')
+#
+#
+# api.ocr_url('URL of image goes here')
+#
+#
+# api.ocr_file('image.jpg')
+# # or:
+# api.ocr_file(open('image.jpg', 'rb'))  # or any other file pointer
+
+# https://api.ocr.space/parse/imageurl?apikey=K89677753388957&url=http://i.imgur.com/fwxooMv.png
+
+# response= requests.get('https://api.ocr.space/parse/imageurl?apikey=K89677753388957&url=http://i.imgur.com/fwxooMv.png')
+# print(response.json())
+# print(response)
+
+
+def ocr_space_file(filename, overlay=False, api_key=apikey, language='eng'):
+    """ OCR.space API request with local file.
+        Python3.5 - not tested on 2.7
+    :param filename: Your file path & name.
+    :param overlay: Is OCR.space overlay required in your response.
+                    Defaults to False.
+    :param api_key: OCR.space API key.
+                    Defaults to 'helloworld'.
+    :param language: Language code to be used in OCR.
+                    List of available language codes can be found on https://ocr.space/OCRAPI
+                    Defaults to 'en'.
+    :return: Result in JSON format.
+    """
+
+    payload = {'isOverlayRequired': overlay,
+               'apikey': api_key,
+               'language': language,
+               }
+    with open(filename, 'rb') as f:
+        r = requests.post('https://api.ocr.space/parse/image',
+                          files={filename: f},
+                          data=payload,
+                          )
+    return r.content.decode()
+
+
+test_file = ocr_space_file(filename='bukti transfer/resi (1).jpeg')
+bersih= test_file.strip()
+pprint(bersih)
